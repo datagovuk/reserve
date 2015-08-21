159051856223d375816873a573aaf1643c0f4287
Ross Jones <ross@servercode.co.uk>
2015-08-21 10:57:43 +0100

    Updated banner insertion to walk the path
    To make sure the banner is on every page, the script now walks the
    path that is provided looking for HTML files to insert into.  If the
    file does not already have a banner, it is inserted.

--- insert_banner.py
+++ insert_banner.py
@@ -1,36 +1,46 @@
 #!/bin/python
 
+"""
+Inserts a banner into every .html file found in the specified directory.
+
+Two arguments to the script are the banner.html file location, and the
+folder containing the html files.
+"""
+
 import sys
 import os.path
 import re
 
-banner_filepath = sys.argv[1]
-with open(banner_filepath, 'rb') as f:
+with open(sys.argv[1], 'rb') as f:
     banner_html = f.read()
-banner_and_body_tag = '{0}</body>'.format(banner_html)
 
+banner_and_body_tag = '{0}</body>'.format(banner_html)
 body_tag_re = re.compile(re.escape("</body>"), re.IGNORECASE)
 
 
-html_filepaths = sys.argv[2:]
-
-for html_filepath in html_filepaths:
-    if not os.path.exists(html_filepath):
-        print 'Cannot find file: %s' % html_filepath
-        sys.exit(1)
-
-    with open(html_filepath, 'rb') as f:
+def insert_banner(filename):
+    """ Insert the banner into the page if it is not already there """
+    with open(filename, 'rb') as f:
         content = f.read()
 
-    if '<!-- Failover banner -->' in content:
-        print 'WARN: Already has a banner - ignoring: %s' % html_filepath
-        continue
+        if '<!-- Failover banner -->' in content:
+            print 'WARN: Already has a banner - ignoring: %s' % filename
+            return
 
-    content, number_of_subs = body_tag_re.subn(banner_and_body_tag, content, re.IGNORECASE)
-    if number_of_subs != 1:
-        print 'ERROR: Wrong num of substitutions (%s) %s' % (number_of_subs, html_filepath)
-        sys.exit(1)
+        content, number_of_subs = body_tag_re.subn(banner_and_body_tag, content, re.IGNORECASE)
+        if number_of_subs != 1:
+            print 'ERROR: Wrong num of substitutions (%s) %s' % (number_of_subs, filename)
+            return
 
-    with open(html_filepath, 'wb') as f:
+    with open(filename, 'wb') as f:
         f.write(content)
-    print 'OK: Written %s' % html_filepath
+
+    print 'OK: Written %s' % filename
+
+
+html_root = sys.argv[2]
+for root, _, files in os.walk(html_root, topdown=False):
+    for name in files:
+        if name.endswith(".html"):
+            insert_banner(os.path.join(root, name))
+
