3927c40cb41942af47cf314ec7ac5e2dc0bf51e1
Ross Jones <ross@servercode.co.uk>
2015-09-01 11:46:09 +0100

    Make the html modder process all the HTML files it can find

--- html_modder.py
+++ html_modder.py
@@ -1,14 +1,15 @@
-#!/bin/python
+#!/usr/bin/env python
 
 import sys
 import os.path
+import os
 import re
 import argparse
 from running_stats import Stats
 
 parser = argparse.ArgumentParser()
-parser.add_argument('banner-filepath', help='Banner HTML')
-parser.add_argument('html-filepaths', nargs="+", help='HTML file to be modified')
+parser.add_argument('banner_filepath', help='Banner HTML')
+parser.add_argument('html_filepath',  help='Folder containing HTML files to be modified')
 parser.add_argument('--search-simple-google', action="store_true", help='Just insert simple google search')
 args = parser.parse_args()
 
@@ -20,53 +21,49 @@ if all_mods:
     banner_and_body_tag = '{0}</body>'.format(banner_html)
     body_tag_re = re.compile(re.escape("</body>"), re.IGNORECASE)
 
-def mod_filepaths(args):
+def mod_filepaths(html_filepath, args):
     stats = Stats()
     are_errors = False
-    for html_filepath in getattr(args, 'html-filepaths'):
-        if not os.path.exists(html_filepath):
-            print 'Cannot find file: %s' % html_filepath
-            sys.exit(1)
 
-        with open(html_filepath, 'rb') as f:
-            content = f.read()
+    with open(html_filepath, 'rb') as f:
+        content = f.read()
 
-        if all_mods:
-            # Banner
-            if '<!-- Failover banner -->' in content:
-                print stats.add('WARN: Already has a banner - ignoring', html_filepath)
+    if all_mods:
+        # Banner
+        if '<!-- Failover banner -->' in content:
+            print stats.add('WARN: Already has a banner - ignoring', html_filepath)
+        else:
+            content, number_of_subs = body_tag_re.subn(banner_and_body_tag, content, re.IGNORECASE)
+            if number_of_subs == 1:
+                print stats.add('Banner added', html_filepath)
             else:
-                content, number_of_subs = body_tag_re.subn(banner_and_body_tag, content, re.IGNORECASE)
-                if number_of_subs == 1:
-                    print stats.add('Banner added', html_filepath)
-                else:
-                    print stats.add('ERROR: Wrong num of banner substitutions', '%s (%s)' % (html_filepath, number_of_subs))
-                    are_errors = True
+                print stats.add('ERROR: Wrong num of banner substitutions', '%s (%s)' % (html_filepath, number_of_subs))
+                are_errors = True
 
-        if all_mods or args.search_simple_google:
-            # Search simple google
-            if '<!-- Simple Google form -->' in content:
-                print stats.add('WARN: Already has google simple form - ignoring', html_filepath)
-            else:
-                content, number_of_subs = search_form_re.subn(simple_google_form_html, content)
-                if number_of_subs == 2:
-                    print stats.add('Main search box on data search page substituted', html_filepath)
-                elif number_of_subs == 1:
-                    print stats.add('Corner search box substituted', html_filepath)
-                elif number_of_subs == 0:
-                    print stats.add('ERROR: Wrong num of corner search (simple google form) substitutions', '%s (%s)' % (html_filepath, number_of_subs))
-                    are_errors = True
+    if all_mods or args.search_simple_google:
+        # Search simple google
+        if '<!-- Simple Google form -->' in content:
+            print stats.add('WARN: Already has google simple form - ignoring', html_filepath)
+        else:
+            content, number_of_subs = search_form_re.subn(simple_google_form_html, content)
+            if number_of_subs == 2:
+                print stats.add('Main search box on data search page substituted', html_filepath)
+            elif number_of_subs == 1:
+                print stats.add('Corner search box substituted', html_filepath)
+            elif number_of_subs == 0:
+                print stats.add('ERROR: Wrong num of corner search (simple google form) substitutions', '%s (%s)' % (html_filepath, number_of_subs))
+                are_errors = True
 
-                content, number_of_subs = search_hint_re.subn('Search site via Google', content)
+            content, number_of_subs = search_hint_re.subn('Search site via Google', content)
 
-        with open(html_filepath, 'wb') as f:
-            f.write(content)
-        print 'OK: Written %s' % html_filepath
+    with open(html_filepath, 'wb') as f:
+        f.write(content)
+    print 'OK: Written %s' % html_filepath
 
     print stats
 
-    if are_errors:
-        sys.exit(1)
+    #if are_errors:
+        #sys.exit(1)
 
 simple_google_form_html = r'''
 <!-- Simple Google form -->
@@ -81,4 +78,11 @@ simple_google_form_html = r'''
 #   <form class="form-search" action="search.html" method="GET">
 search_form_re = re.compile(r'<form ([^>]*)action="(?:search.html|\/data\/search)"([^>]*)>')
 search_hint_re = re.compile('Search for data\.\.\.')
-mod_filepaths(args)
+
+print args
+for root, _, files in os.walk(args.html_filepath, topdown=False):
+    for name in files:
+        if name.endswith(".html"):
+            mod_filepaths(os.path.join(root, name), args)
+
+
--- scrape.sh
+++ scrape.sh
@@ -10,12 +10,16 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Scrape the webpages into a specific directory
 echo "+ Scraping site"
-wget -P static_site -X "/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
+#wget -P static_site -X "/data/resource_cache*,/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
+wget -P static_site --reject "user*,search*" -X "/data/resource_cache*,/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
 
 # Mod the HTML to insert a banner and fix search etc
 echo "+ Inserting banner"
-python $DIR/html_modder.py $DIR/banner.html $DIR/static_site
+python $DIR/html_modder.py $DIR/banner.html $DIR/static_site --search-simple-google
+
 
 # Sync with S3
 echo "+ Syncing with S3"
 s3cmd sync -v $DIR/static_site/$RESERVE_SITE  s3://data.gov.uk/
+
+
