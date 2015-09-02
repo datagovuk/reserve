d584ce54ce9f9e11f8937c4917c3393f6773210b
Ross Jones <ross@servercode.co.uk>
2015-09-02 11:47:43 +0100

    PUT content, don't SYNC as we don't want to download anything

--- scrape.sh
+++ scrape.sh
@@ -11,7 +11,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Scrape the webpages into a specific directory
 echo "+ Scraping site"
 #wget -P static_site -X "/data/resource_cache*,/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
-wget -P static_site --reject "user*,search*" -X "/data/resource_cache*,/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
+wget -P static_site --reject "user*,search@*" -X "/data/resource_cache*,/data/search*,/api/*,/search*,/user*,/feeds*,/flag*,/vote*"  --adjust-extension -p --convert-links --restrict-file-names=windows -m -e robots=off  --wait .5 -x http://$RESERVE_SITE
 
 # Mod the HTML to insert a banner and fix search etc
 echo "+ Inserting banner"
@@ -20,6 +20,6 @@ python $DIR/html_modder.py $DIR/banner.html $DIR/static_site --search-simple-goo
 # Sync with S3
 echo "+ Syncing with S3"
 cd $DIR/static_site/$RESERVE_SITE
-s3cmd sync -v .  s3://data.gov.uk/
+s3cmd put -r . s3://data.gov.uk/
 
 
